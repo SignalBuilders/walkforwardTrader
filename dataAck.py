1fa7bc101b121c02a6ac7253297032ba726796c8
patrick-ogrady <pogrady@stanford.edu>
2017-08-21 21:19:04 -0700

    small optimizations at a time

--- dataAck.py
+++ dataAck.py
@@ -593,7 +593,7 @@ class endToEnd:
                     preds.append(returnDict[i])
                     actuals.append(yVals[int(i) + 44])
                     days.append(yIndex[int(i) + 44])
-
+                
                 print("LOSS", log_loss(np.array(endToEnd.transformTargetArr(np.array(actuals), self.threshold)), np.array(preds)))
                 print("ROC AUC", roc_auc_score(np.array(endToEnd.transformTargetArr(np.array(actuals), self.threshold)), np.array(preds)))
                 print("ACCURACY", accuracy_score(np.array(endToEnd.transformTargetArr(np.array(actuals), self.threshold)), np.array(preds).round()))
@@ -604,7 +604,7 @@ class endToEnd:
                     predsTable = predsTable.join(predsTable.shift(i), rsuffix="_" + str(i))
                     i += 1
                 
-                transformedPreds = pd.DataFrame(predsTable.apply(lambda x:computePosition(x), axis=1), columns=["Predictions"]).dropna()
+                transformedPreds = pd.DataFrame(predsTable.apply(lambda x:computePositionConfidence(x), axis=1), columns=["Predictions"]).dropna()
                 dailyFactorReturn = getDailyFactorReturn(self.walkForward.targetTicker, dataOfInterest)
                 transformedPreds = transformedPreds.join(dailyFactorReturn).dropna()
                 returnStream = pd.DataFrame(transformedPreds.apply(lambda x:x[0] * x[1], axis=1), columns=["Algo Return"]) if returnStream is None else pd.concat([returnStream, pd.DataFrame(transformedPreds.apply(lambda x:x[0] * x[1], axis=1), columns=["Algo Return"])])
