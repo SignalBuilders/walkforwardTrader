8269da64fb7794b6c0356c71ae2fea2c7f1f605d
patrick-ogrady <pogrady@stanford.edu>
2017-08-21 19:27:23 -0700

    debugging implementation

--- modelSearch.py
+++ modelSearch.py
@@ -52,45 +52,45 @@ while True:
             s = sManager.createSeries()
             
 
-        try:
-            for defaultWindowSize in [5, 10, 22, 44]:
-                for trees in [25, 50, 100, 150, 300]:
-                    for predictionLength in [5, 7, 10, 15]:
-                        if random.uniform(0,1) < 0.7: ##RANDOMLY SKIP
-                            continue
-                        b = dataAck.algoBlob(s, defaultWindowSize, trees, predictionLength, tickerToTrade)
-                        algoReturn, factorReturn, predictions, slippageAdjustedReturn =  b.makePredictions(portfolio.prepareDataForModel(b, joinedData), daysToCheck = None, earlyStop = True)
-                        if algoReturn is None:
-                            toLog = factorReturn
-                            if np.isnan(toLog["sharpe"]) == True:
-                                raise ValueError('SHARPE IS NAN SO FAULTY SERIES')
-                                
-                            toLog["modelDescription"] = str(b.describe())
-                            dataAck.logModel("Model Stopped Early", toLog)
-                            print("Model Stopped Early", toLog)
-                            continue
-                        metrics = dataAck.vizResults(slippageAdjustedReturn[:-252], algoReturn[:-252], factorReturn[:-252], False)
-                        print("TRAIN:", metrics)
-                        if np.isnan(metrics["SHARPE"]) == True:
+        # try:
+        for defaultWindowSize in [5, 10, 22, 44]:
+            for trees in [25, 50, 100, 150, 300]:
+                for predictionLength in [5, 7, 10, 15]:
+                    if random.uniform(0,1) < 0.7: ##RANDOMLY SKIP
+                        continue
+                    b = dataAck.algoBlob(s, defaultWindowSize, trees, predictionLength, tickerToTrade)
+                    algoReturn, factorReturn, predictions, slippageAdjustedReturn =  b.makePredictions(portfolio.prepareDataForModel(b, joinedData), daysToCheck = None, earlyStop = True)
+                    if algoReturn is None:
+                        toLog = factorReturn
+                        if np.isnan(toLog["sharpe"]) == True:
                             raise ValueError('SHARPE IS NAN SO FAULTY SERIES')
-                        if metrics["ACTIVITY"] > 0.7 and metrics["BETA"] < 0.6\
-                             and metrics["SHARPE DIFFERENCE SLIPPAGE"] > 0.0 and metrics["ALPHA SLIPPAGE"] > 0.03:
-                            ##STORE
-                            testMetrics = dataAck.vizResults(slippageAdjustedReturn[-252:], algoReturn[-252:], factorReturn[-252:], False)
-                            print("TEST:", testMetrics)
-                            print("TODAY:", b.makeTodayPrediction(portfolio.prepareDataForModel(b, joinedData)))
-                            dataAck.storeModelData(b, algoReturn, predictions, slippageAdjustedReturn)
-                            dataAck.storeModel(b, metrics, testMetrics)
-                        else:
-                            toLog = {"modelDescription":str(b.describe())}
-                            for k in metrics:
-                                toLog[k] = metrics[k]
-                            dataAck.logModel("Model Skipped", toLog)
-        except:
-            print("FAILED", s.describe())
-            dataAck.logModel("Series Failed", {
-                "seriesDescription":str(s.describe())
-            })
+                            
+                        toLog["modelDescription"] = str(b.describe())
+                        dataAck.logModel("Model Stopped Early", toLog)
+                        print("Model Stopped Early", toLog)
+                        continue
+                    metrics = dataAck.vizResults(slippageAdjustedReturn[:-252], algoReturn[:-252], factorReturn[:-252], False)
+                    print("TRAIN:", metrics)
+                    if np.isnan(metrics["SHARPE"]) == True:
+                        raise ValueError('SHARPE IS NAN SO FAULTY SERIES')
+                    if metrics["ACTIVITY"] > 0.7 and metrics["BETA"] < 0.6\
+                         and metrics["SHARPE DIFFERENCE SLIPPAGE"] > 0.0 and metrics["ALPHA SLIPPAGE"] > 0.03:
+                        ##STORE
+                        testMetrics = dataAck.vizResults(slippageAdjustedReturn[-252:], algoReturn[-252:], factorReturn[-252:], False)
+                        print("TEST:", testMetrics)
+                        print("TODAY:", b.makeTodayPrediction(portfolio.prepareDataForModel(b, joinedData)))
+                        dataAck.storeModelData(b, algoReturn, predictions, slippageAdjustedReturn)
+                        dataAck.storeModel(b, metrics, testMetrics)
+                    else:
+                        toLog = {"modelDescription":str(b.describe())}
+                        for k in metrics:
+                            toLog[k] = metrics[k]
+                        dataAck.logModel("Model Skipped", toLog)
+        # except:
+            # print("FAILED", s.describe())
+            # dataAck.logModel("Series Failed", {
+            #     "seriesDescription":str(s.describe())
+            # })
 
         runsSeen += 1
 
