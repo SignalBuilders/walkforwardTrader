5409f7f145279bce94a7534e0160e1d0bdf423cf
patrick-ogrady <pogrady@stanford.edu>
2017-08-22 19:25:28 -0700

    dont log failed early

--- TreePredictor.py
+++ TreePredictor.py
@@ -35,6 +35,7 @@ class TreePredictor:
         return (self.obj1.describe(), self.obj2.describe(), self.predictionDistance, self.combiner)
 
     def getHash(self):
+        print("HASH STRING", self.describe())
         return hashlib.sha224(str(self.describe()).encode('utf-8')).hexdigest()
 
     def formUploadDictionary(self):
