4f9b239af59ae3fcee2d686358a2ae6c8d84911f
patrick-ogrady <pogrady@stanford.edu>
2017-08-23 15:10:59 -0700

    typo

---
+++ curveSearch.py
@@ -0,0 +1,137 @@
+import dataAck
+import portfolio
+import CurvePredictor
+import TreePredictor
+import curveTreeDB
+import params
+from google.cloud import error_reporting
+
+client = error_reporting.Client('money-maker-1236', service="Curve Search", version=params.curveAndTreeVersion)
+try:
+    allTickers = dataAck.getAllTickersPlain()
+    while True:
+        import random
+        ##ADVANCED TICKER TO TRADE SELECTION
+        modelCount, modelSplitByTicker, predictionCount, numPredictors = curveTreeDB.getModelCounts(params.curveModels)
+
+        validTickersToTrade = []
+
+        for ticker in allTickers:
+            if ticker not in modelSplitByTicker:
+                validTickersToTrade.append(ticker)
+                print("NOT PRESENT", ticker)
+
+        if len(validTickersToTrade) == 0:
+            ##MEANS ALL TICKERS HAVE AT LEAST ONE MODEL
+            for ticker in sorted(modelSplitByTicker, key=modelSplitByTicker.get)[:15]:
+                validTickersToTrade.append(ticker)
+                print(ticker, modelSplitByTicker[ticker])
+
+
+
+        tickerToTrade = validTickersToTrade[random.randint(0, len(validTickersToTrade)) - 1]
+        print(tickerToTrade)
+        
+        tData = dataAck.getTrainingData(tickerToTrade)
+        joinedData = None
+        validTickers = None
+
+        
+        
+        if tData is None:
+            dataAck.logModel("Cache", {
+                "type":"miss",
+                "ticker":tickerToTrade,
+                "day":str(portfolio.getToday())
+            })
+
+            tickersToPull = dataAck.getDataSourcesForTicker(tickerToTrade)
+            print(tickersToPull)
+
+            pulledData, validTickers = dataAck.downloadTickerData(tickersToPull)
+
+            joinedData = dataAck.joinDatasets([pulledData[ticker] for ticker in pulledData])
+            
+            dataAck.storeTrainingData(tickerToTrade, (joinedData, validTickers))
+        else:
+            joinedData = tData[0]
+            validTickers = tData[1]
+            dataAck.logModel("Cache", {
+                "type":"hit",
+                "ticker":tickerToTrade,
+                "day":str(portfolio.getToday())
+            })
+            
+        
+        sManager = dataAck.seriesManager(validTickers)
+        print(sManager.describe())
+
+        import time
+        import warnings
+        import numpy as np
+        warnings.filterwarnings("ignore")
+        ##GET ALGOS INITIALLY GOOD
+        runsSeen = 0
+        while True:
+            s = sManager.createSeries()
+            while s.checkValidity(s.transformJoinedData(joinedData[:"2016-01-01"])) == False:
+                s = sManager.createSeries()
+            
+            print("*********")
+            print("NEW SERIES", s.describe())
+            print("*********")
+
+            try:
+                for lookback in [5, 10, 22, 44]:
+                    for prediction in [2, 3, 5, 7, 10, 15]:
+                        for radius in [0.3, 0.5, 0.7, 1.0, 1.5, 2.0]:
+                            for minConfidence in [0.01, 0.05, 0.1, 0.2]:
+                                for minNeighbors in [10]:
+                                    if random.uniform(0,1) < 0.97: ##RANDOMLY SKIP A LOT...FAILING FAST ON SERIES ALLOWS US TO EXAMINE MUCH LARGER SAMPLE SPACE
+                                        continue
+                                    cPre = CurvePredictor.CurvePredictor(s, tickerToTrade, lookback, prediction, radius, minConfidence, minNeighbors)
+                                    algoReturn, factorReturn, predictions, slippageAdjustedReturn, rawPredictions = cPre.runModelHistorical(joinedData, earlyStop=True)
+                                    if algoReturn is None:
+                                        toLog = factorReturn
+                                        toLog["modelDescription"] = str(cPre.describe())
+                                        # dataAck.logModel("Model Curve Stopped Early", toLog)
+                                        print("Model Curve Stopped Early", toLog)
+                                        continue
+                                    metrics = dataAck.vizResults(slippageAdjustedReturn[:-252], algoReturn[:-252], factorReturn[:-252], False)
+                                    print("TRAIN:", metrics)
+                                    if metrics["RAW BETA"] < 0.6\
+                                         and (metrics["SHARPE"] > 0.35 or metrics["SHARPE DIFFERENCE"] > 0.0) and metrics["ACTIVITY"] > 0.2:
+                                        ##STORE
+                                        testMetrics = dataAck.vizResults(slippageAdjustedReturn[-252:], algoReturn[-252:], factorReturn[-252:], False)
+                                        print("TEST:", testMetrics)
+                                        curveTreeDB.storeModel(params.curveModels, cPre, cPre.formUploadDictionary(), metrics, testMetrics)
+                                    else:
+                                        toLog = {"modelDescription":str(cPre.describe())}
+                                        for k in metrics:
+                                            toLog[k] = metrics[k]
+                                        dataAck.logModel("Model Curve Skipped", toLog)
+            except:
+                print("FAILED", s.describe())
+                dataAck.logModel("Series Failed", {
+                    "seriesDescription":str(s.describe())
+                })
+
+
+
+            runsSeen += 1
+
+            if runsSeen > 20:
+                ##START NEW TICKER
+                dataAck.logModel("Search Update", {
+                    "message":"restarting search with different ticker",
+                    "currentTicker":tickerToTrade
+                })
+                break
+
+except:
+    client.report_exception()
+
+
+
+
+
