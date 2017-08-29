1c18c7ad5419062b3796908ec9faceb2ff55cbb5
patrick-ogrady <pogrady@stanford.edu>
2017-08-29 11:20:37 -0700

    better logging

--- fastAutomaticPortfolioTrader.py
+++ fastAutomaticPortfolioTrader.py
@@ -365,7 +365,7 @@ def performPortfolioPerformanceEstimation(historicalPredictions, historicalRetur
             weightsSeen = pd.DataFrame(returnWindow.apply(lambda x: weightArray, axis=1, raw=True), columns=returnWindow.columns.values)
             
         
-        
+        print(weightsSeen)
         if historicalWeights is None:
             historicalWeights = weightsSeen
         else:
