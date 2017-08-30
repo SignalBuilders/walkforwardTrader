1b60cc0d2e6fc87d7e2d3cd7562cd67a39048b26
patrick-ogrady <pogrady@stanford.edu>
2017-08-30 10:14:57 -0700

    trying mp

--- fastAutomaticPortfolioTrader.py
+++ fastAutomaticPortfolioTrader.py
@@ -323,7 +323,6 @@ def produceSingleHRP(identifier, aggregateReturns, sharedDict):
         sumReturn = pd.DataFrame(todayReturn.apply(lambda x:sum(x), axis=1))
         thisWeights = pd.DataFrame([[weights[item] for item in weights.index]], index=sumReturn.index, columns=weights.index.tolist())
         sharedDict[identifier] = thisWeights
-        print(thisWeights)
     except:
         sharedDict[identifier] = None
 
